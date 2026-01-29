7130f3c5b22e6d3394ab7bb4b0554646f4a55320
3bdo7amouda <abdelrahman.hamouda29@gmail.com>
2026-01-29 21:56:56 +0000

    Add OIDC issuer URL output and format cluster CA output for consistency

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -19,10 +19,14 @@ output "cluster_endpoint" {
 }
 
 output "cluster_ca" {
-  value = module.eks.cluster_certificate_authority_data
+  value     = module.eks.cluster_certificate_authority_data
   sensitive = true
 }
 
 output "cluster_security_group_id" {
   value = module.eks.cluster_security_group_id
+}
+
+output "oidc_issuer_url" {
+  value = module.eks.oidc_issuer_url
 }
