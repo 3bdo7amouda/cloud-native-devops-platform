681113ef1cd71f064d7972b812de623cc882dce9
3bdo7amouda <abdelrahman.hamouda29@gmail.com>
2026-01-29 12:33:43 +0000

    Update outputs to reference the vpc module instead of networking

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = module.networking.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
-  value = module.networking.public_subnet_ids
+  value = module.vpc.public_subnet_ids
 }
 
 output "private_subnet_ids" {
-  value = module.networking.private_subnet_ids
+  value = module.vpc.private_subnet_ids
 }
