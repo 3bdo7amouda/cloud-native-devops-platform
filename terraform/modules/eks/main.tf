136beb7b35a33bcbe67f0b24eab8366e9b09e2cb
3bdo7amouda <abdelrahman.hamouda29@gmail.com>
2026-01-31 09:51:54 +0000

    Remove addon_version from vpc_cni and count condition from ebs_csi in EKS module

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -42,7 +42,6 @@ resource "aws_eks_node_group" "this" {
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name  = aws_eks_cluster.this.name
   addon_name    = "vpc-cni"
-  addon_version = "v1.21.1-eksbuild.3"
 }
 
 resource "aws_eks_addon" "coredns" {
@@ -56,8 +55,6 @@ resource "aws_eks_addon" "kube_proxy" {
 }
 
 resource "aws_eks_addon" "ebs_csi" {
-  count = var.ebs_csi_service_account_role_arn == null ? 0 : 1
-
   cluster_name             = aws_eks_cluster.this.name
   addon_name               = "aws-ebs-csi-driver"
   service_account_role_arn = var.ebs_csi_service_account_role_arn
