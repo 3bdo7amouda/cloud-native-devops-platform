1c66eab998a2eedf101e8ff1e1e2b20e9f55e122
3bdo7amouda <abdelrahman.hamouda29@gmail.com>
2026-02-07 20:26:11 +0000

    fix: update EKS managed add-ons to include conflict resolution on create and update

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,36 +43,40 @@ module "eks" {
   capacity_type                  = var.capacity_type
   cluster_access_entries         = var.cluster_access_entries
   tags                           = var.tags
+}
 
-  # EKS managed add-ons
-  resource "aws_eks_addon" "vpc_cni" {
-    cluster_name             = module.eks.cluster_name
-    addon_name               = "vpc-cni"
-    resolve_conflicts_on_update = "OVERWRITE"
-    tags                       = var.tags
-  }
+# Standard EKS managed add-ons
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name                = module.eks.cluster_name
+  addon_name                  = "vpc-cni"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  tags                        = var.tags
+}
 
-  resource "aws_eks_addon" "kube_proxy" {
-    cluster_name             = module.eks.cluster_name
-    addon_name               = "kube-proxy"
-    resolve_conflicts_on_update = "OVERWRITE"
-    tags                       = var.tags
-  }
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name                = module.eks.cluster_name
+  addon_name                  = "kube-proxy"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  tags                        = var.tags
+}
 
-  resource "aws_eks_addon" "coredns" {
-    cluster_name             = module.eks.cluster_name
-    addon_name               = "coredns"
-    resolve_conflicts_on_update = "OVERWRITE"
-    tags                       = var.tags
-  }
+resource "aws_eks_addon" "coredns" {
+  cluster_name                = module.eks.cluster_name
+  addon_name                  = "coredns"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  tags                        = var.tags
+}
 
-  resource "aws_eks_addon" "ebs_csi" {
-    cluster_name             = module.eks.cluster_name
-    addon_name               = "aws-ebs-csi-driver"
-    service_account_role_arn = module.irsa.irsa_role_arns["ebs_csi"]
-    resolve_conflicts_on_update = "OVERWRITE"
-    tags                       = var.tags
-  }
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name                  = module.eks.cluster_name
+  addon_name                    = "aws-ebs-csi-driver"
+  service_account_role_arn      = module.irsa.irsa_role_arns["ebs_csi"]
+  resolve_conflicts_on_create   = "OVERWRITE"
+  resolve_conflicts_on_update   = "OVERWRITE"
+  tags                          = var.tags
 }
 
 module "irsa" {
