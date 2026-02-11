1c325c289330bcafd7b8964b5f7bf9216428f199
3bdo7amouda <abdelrahman.hamouda29@gmail.com>
2026-02-11 17:07:18 +0000

    feat: add service account and image pull secrets support to deployments; update values.yaml

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -110,7 +110,7 @@ resource "aws_eks_node_group" "platform" {
   )
 
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy = false
     ignore_changes        = [scaling_config[0].desired_size]
   }
 
